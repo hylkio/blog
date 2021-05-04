f63a2af13c5e69ebbb391d6688359e433cc6cc9c
Someone <mirosopa@gmail.com>
2021-05-04 10:54:14 +0300

    try something new

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Sopanem Blog', // Required
+  author: 'Miro Sopanen', // Required
+  description: 'Full-stack Developer & Freelancer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://sopanem.github.io',
+    github: 'https://github.com/sopanem',
+    twitter: 'https://twitter.com/sopanenm',
+    // linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://sopanem.github.io/blog/',
+};
